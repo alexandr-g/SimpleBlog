84e973c0e85f16382863d602172591faf9f6634a
Alexandr <alexgrischuk@gmail.com>
2015-07-29 00:19:38 +0300

    Cucumber test refactor

--- features/create_new_blog_post.feature
+++ features/create_new_blog_post.feature
@@ -1,6 +1,6 @@
 Feature: Go to blog page and create new blog post
 
-Scenario: Admin visits blog website and create new Post       
+Scenario: Admin visits blog website and create new Post
 	Given I am on the Blog homepage
 	And I will navigate to "Manage Posts"
 	And I will add "New Post"
---
+++ features/step_definitions/create_new_blog_post.rb
@@ -0,0 +1,17 @@
+  Given(/^I am on the Blog homepage$/) do
+    visit 'http://localhost:3000/'
+  end
+
+  And(/^I will navigate to "(.*?)"$/) do |arg1|
+  	click_on('Manage Posts')
+  end
+
+  Given(/^I will add "(.*?)"$/) do |arg1|
+  	find('#new_post', :visible => true).click
+  end
+
+  Then(/^I will fill the form$/) do
+    fill_in('Title', :with => 'Test Automation with Cucumber')
+    fill_in('Content', :with => 'This post was created automatically. Please, ignore.')
+    click_button('Submit')
+  end
